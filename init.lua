578a3925b9486004f714661d810790e985fe7415
Victor Hedegran <victor.hedegran@gmail.com>
2026-02-28 20:05:47 +0100

    add ts_ls and copilot

--- init.lua
+++ init.lua
@@ -602,7 +602,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -612,12 +612,11 @@ require('lazy').setup({
       --    :Mason
       --
       -- You can press `g?` for help in this menu.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
+      local ensure_installed = {
         'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
-        -- You can add other tools here that you want Mason to install
-      })
+        'typescript-language-server', -- You can add other tools here that you want Mason to install
+      }
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -888,6 +887,22 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          keymap = {
+            accept = "<C-l>", -- IMPORTANT (avoid Tab conflict)
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
